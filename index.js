6fe3a4ebe27fd69a17825df1d2500e85ce961434
AnActualEmerald <emerald_actual@protonmail.com>
2022-07-29 17:00:47 -0400

    Finish env setup

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const path = require('path');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const tc = require('@actions/tool-cache');
+const fs = require('fs');
+const {exec} = require('child_process');
+const http = require('http');
 
 const download_ext = {
   "linux":"linux-x64.tar.gz",
@@ -36,16 +39,90 @@ async function setup() {
 async function main() {
   // Set up environment
   core.startGroup("Set up environment");
-  let p = core.getInput('path');
+  let p = core.toPlatformPath(core.getInput('path'));
   if (!p) {
     p = ".";
     core.info("Path input not set");
   }
+
+  const wrap = core.getInput('wrap');
+  const target = core.toPlatformPath(path.join('/dist', wrap ? wrap : ''));
+
+  //Create the dist directory
+  fs.mkdir(target, {recursive: true});
+
+  //Check for the README.md
+  if  (fs.existsSync(path.join(p, "README.md"))){
+    core.info(`Using README in ${p}`);
+    fs.renameSync(path.join(p, "README.md"), core.toPlatformPath(path.join("/", "README.md")));
+  }else if (core.getInput("readme") !== "") {
+    const url = core.getInput("readme");
+    core.info(`Attempting to download README from ${url}`);
+    donwloadFile(url, "/README.md");
+  }
+
+
+  //Check for the icon
+  if (fs.existsSync(path.join(p, "icon.png"))){
+    core.info(`Using icon in ${p}`);
+    fs.renameSync(path.join(p, "icon.png"), core.toPlatformPath(path.join("/", "icon.png")));
+  }else if (core.getInput("icon") !== "") {
+    const url = core.getInput("icon");
+    core.info(`Attempting to download icon from ${url}`);
+    donwloadFile(url, "/icon.png");
+  }
+  fs.renameSync(p, target);
+
+  core.endGroup();
+
+  core.startGroup("Configre cli");
+  process.chdir("/");
+
+  const name = core.getInput('name', {required = true});
+  const namespace = core.getInput('namespace', {required = true});
+  const version = core.getInput('version', {required = true});
+
+  exec(`tcli init --package-name=${name} --package-namespace=${namespace} --package-version=${version}`, (err, stdout, stderr) => {
+    if (err) {
+      core.error("Error calling the Thunderstore CLI");
+      core.error(err);
+      core.setFailed();
+    }
+
+    if (stderr) {
+      core.error("Error from tcli");
+      core.error(stderr);
+      core.setFailed();
+    }
+
+    core.info(stdout);
+
+  });
+
+
+
   
   
 }
 
+async function donwloadFile(url, name) {
+  const file = fs.createWriteStream(name);
+  http.get(url, (resp) => {
+    resp.pipe(file);
+
+    file.on("finish", () => {
+      file.close();
+    });
+
+    file.on("error", (e) => {
+      core.debug(e);
+      core.setFailed(`Error when writing file ${name}`);
+    });
+  });
+}
+
 if (require.main === module) {
-  setup();
-  main();
+  //Idk if github actions have top level await or not
+  setup().then();
+  main().then(() => core.info("Done!"));
 }
